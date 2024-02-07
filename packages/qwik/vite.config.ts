7ddda088f04ff86c4fc96adab283eca3411744ee
dsod <daniel.soderling@gmail.com>
2024-02-08 00:02:19 +0100

    pipeline fix

--- packages/qwik/vite.config.ts
+++ packages/qwik/vite.config.ts
@@ -3,7 +3,11 @@ import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import { defineConfig } from "vite";
 import pkg from "./package.json";
 
-const { dependencies = {}, peerDependencies = {} } = pkg as any;
+type Packages = typeof pkg & {
+	dependencies: Record<string, string>;
+	peerDependencies: Record<string, string>;
+};
+const { dependencies = {}, peerDependencies = {} } = pkg as Packages;
 const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
 const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 
